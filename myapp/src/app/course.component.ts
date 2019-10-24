873f3bf1448f6dc30ecfa80234e4080d6d90965e
Varsha Katkar <varsha.katkar93@gmail.com>
2019-10-24 13:19:23 +0530

    #Varsha : Style & Class property binding, Event binding

--- myapp/src/app/course.component.ts
+++ myapp/src/app/course.component.ts
@@ -1,21 +1,66 @@
+// [style.color]="isActive ? 'green' : 'orange' "
+// [style.fontSize]="isActive ? '45px' : '20px' "
+// [class.content]="isActive"
+// [class.item]="!isActive"
+
 import {Component} from '@angular/core';
 import {Courses} from './courses';
 
 @Component({
     selector : 'app-course',
     template : `
-    <h1>{{userName}}</h1>
+    <h1
+    [ngStyle] = "{
+        color : isActive ? 'green' : 'orange',
+        fontSize : isActive ? '45px' : '20px'
+        }"
+    (click) = "Action()"
+    >{{userName}}</h1>
     <ul>
         <li *ngFor="let course of courseList">
              {{course}}
         </li>
     </ul>
+
+    <div
+    [ngClass]="{
+        'content' : isActive,
+        'item' : !isActive
+    }"
+    ></div>
+
+    <span class="fa fa-3x"
+    (click)="HeartLike()"
+    [ngClass]="{
+        'fa-heart' : activeHeart,
+        'fa-heart-o' : !activeHeart
+    }"
+    ></span>
+
+
     `,
-    styles : [``],
+    styles : [`
+    h1{
+        color : green;
+    }
+    .content{
+        width : 400px;
+        height: 400px;
+        background-color: teal;
+    }
+    .item{
+        width : 200px;
+        height: 200px;
+        background-color:yellow;
+    }
+    
+    `]
     
 })
 
 export class CourseComponent{
+    public isActive : boolean = false;
+    public activeHeart : boolean = false;
     public userName : string = 'Varsha Katkar';
     public courseList : Array<string>;
     // public courses : Array<string> = ['Angular','Bootstrap','TypeScript','JavaScript'];
@@ -25,5 +70,13 @@ export class CourseComponent{
         // let course = new Courses();
         // this.courseList = course.getCourse();
         this.courseList = course.getCourse();
+    }
+
+    Action(){
+        this.isActive = !this.isActive;
+    }
+
+    HeartLike(){
+        this.activeHeart = !this.activeHeart; 
     }
 }
