5e102fd0ca1fbc3e702fb93f8babfdd106a77a44
Varsha Katkar <varsha.katkar93@gmail.com>
2019-12-18 15:48:19 +0530

    Reactive Extension(RXJS) in JS and Angular

--- myapp/src/app/home/home.component.ts
+++ myapp/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import {fromEvent} from 'rxjs';
+import {map,filter,debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el : ElementRef) { }
 
   ngOnInit() {
+    let searchIn = this.el.nativeElement.querySelector('#search1');
+    let observe = fromEvent(searchIn,'keyup')
+                        .pipe(map((e:any) => e.target.value),
+                              filter(text => text.length >= 4),
+                              debounceTime(2000)
+                             );
+
+    observe.subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
